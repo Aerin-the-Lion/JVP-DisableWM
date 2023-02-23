2d147ff86b96a9479d6156931364d7fae67b7cdf
Aerin the Lion <96946287+Aerin-the-Lion@users.noreply.github.com>
2023-02-23 19:39:22 +0900

    Add files via upload

---
+++ DisableWM/DisableWM/DisableWM.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using HarmonyLib;
+using UnityEngine.UI;
+using System.Runtime.CompilerServices;
+using System.Reflection;
+
+namespace DisableWM
+{
+    public class DisableWM : MonoBehaviour
+    {
+        static bool isInject;
+        //static GameObject GUI_Camera;
+        //static GameObject addText;
+        [HarmonyPostfix, HarmonyPatch(typeof(WmController), "Update")]
+        static void InjectCode() {
+            if(!Main.CFG_IS_ENABLED.Value) { return; }
+            Debug.Log("isInject " + isInject);
+            Debug.Log("!!!! INJECT CODE STARTED! !!!!");
+            if (isInject) { return; }
+            //javText = GameObject.Find("Gui Camera").GetComponent<TMPro.TextMeshProUGUI>();
+            GameObject GUI_Camera = GameObject.Find("Gui Camera");
+            GameObject addText = GUI_Camera.transform.Find("addText").gameObject;
+            TMPro.TextMeshProUGUI TextMeshProUGUI = addText.GetComponent<TMPro.TextMeshProUGUI>();
+            if (TextMeshProUGUI.enabled)
+            {
+                TextMeshProUGUI.enabled = false;
+            }
+            isInject = true;
+            Debug.Log("!!!! INJECT CODE ENDED! !!!!");
+        }
+
+    }
+}
---
+++ DisableWM/DisableWM/Main.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BepInEx;
+using BepInEx.Configuration;
+using UnityEngine;
+using HarmonyLib;
+using UnityEngine.UI;
+
+namespace DisableWM
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    [BepInProcess("JavPlayer.exe")]
+    public class Main : BaseUnityPlugin
+    {
+        public const string PluginGuid = "me.NoName.JavPlayer.v112b.DisableWM";
+        public const string PluginName = "DisableWM";
+        public const string PluginVersion = "1.0.0.0";
+        public static ConfigEntry<bool> CFG_IS_ENABLED { get; private set; }
+
+        public void LoadConfig()
+        {
+            string textIsEnable = "0. MOD Settings";
+            CFG_IS_ENABLED = Config.Bind<bool>(textIsEnable, "Activate the MOD", true, "If you need to disable the mod, Toggle to Disabled");
+        }
+
+        void Awake()
+        {
+            LoadConfig();
+            Harmony.CreateAndPatchAll(typeof(DisableWM));
+        }
+        void Update()
+        {
+            //UpdateCount++;
+            //Debug.Log("Update Count : " + UpdateCount);
+        }
+
+    }
+}
