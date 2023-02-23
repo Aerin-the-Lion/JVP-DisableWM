e9f294a8625a9d3255bf4bd659483ad210c87ae4
Aerin the Lion <96946287+Aerin-the-Lion@users.noreply.github.com>
2023-02-23 19:56:02 +0900

    Update DisableWM.cs

--- DisableWM/DisableWM/DisableWM.cs
+++ DisableWM/DisableWM/DisableWM.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -21,19 +21,17 @@ namespace DisableWM
         [HarmonyPostfix, HarmonyPatch(typeof(WmController), "Update")]
         static void InjectCode() {
             if(!Main.CFG_IS_ENABLED.Value) { return; }
-            Debug.Log("isInject " + isInject);
-            Debug.Log("!!!! INJECT CODE STARTED! !!!!");
             if (isInject) { return; }
             //javText = GameObject.Find("Gui Camera").GetComponent<TMPro.TextMeshProUGUI>();
             GameObject GUI_Camera = GameObject.Find("Gui Camera");
-            GameObject addText = GUI_Camera.transform.Find("addText").gameObject;
+            GameObject Canvas = GUI_Camera.transform.Find("Canvas").gameObject;
+            GameObject addText = Canvas.transform.Find("addText").gameObject;
             TMPro.TextMeshProUGUI TextMeshProUGUI = addText.GetComponent<TMPro.TextMeshProUGUI>();
             if (TextMeshProUGUI.enabled)
             {
                 TextMeshProUGUI.enabled = false;
             }
             isInject = true;
-            Debug.Log("!!!! INJECT CODE ENDED! !!!!");
         }
 
     }
